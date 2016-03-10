fb235b726581f8b1740b03a6bbd244869597b678
nictedesco <n.nicolatedesco@gmail.com>
2016-03-10 11:34:56 +0100

    Added log levels

--- Logger.php
+++ Logger.php
@@ -1,6 +1,22 @@
 <?php
 
 /**
+ * Log levels
+ */
+abstract class logLevel
+{
+  const INFO  = 0;
+  const WARN  = 1;
+  const ERROR = 2;
+  const DEBUG = 3;
+
+  const strInfo   = 'INFO';
+  const strWarn   = 'WARNING';
+  const strError  = 'ERROR';
+  const strDebug  = 'DEBUG';
+}
+
+/**
  * Basic log class
  */
 class Logger
@@ -14,6 +30,11 @@ class Logger
    * Actual Date and time
    */
   private static $date = null;
+
+  /**
+   * Log level
+   */
+  private static $logLevel = null;
 
   /**
    * Getters and setters
@@ -38,6 +59,16 @@ class Logger
     self::$date = date_create(null, timezone_open('UTC'));
   }
 
+  public static function getLogLevel()
+  {
+    return self::$logLevel;
+  }
+
+  public static function setLogLevel($logLevel)
+  {
+    self::$logLevel = $logLevel;
+  }
+
   /**
    * Constructor
    */
@@ -56,19 +87,48 @@ class Logger
   	return date_format(date_create(null, timezone_open('UTC')), 'Y.m.d H:i:s P');
   }
 
-  private static function buildLogMessage($message)
+  private static function buildLogMessage($level, $message)
   {
   	if (null !== self::getDate()) {
   		self::setDate();
   	}
 
-  	return self::getFormattedDate() . ' - ' . $message;
+  	//return self::getFormattedDate() . ' - ' .$level . ' - ' . $message;
+    return self::getFormattedDate() . " - {$level} - {$message}";
   }
 
-  public static function log($message)
+  public static function info($message)
+  {
+    if (self::getLogLevel() >= logLevel::INFO) {
+      return self::log(logLevel::strInfo, $message);
+    }
+  }
+
+  public static function warn($message)
+  {
+    if (self::getLogLevel() >= logLevel::WARN) {
+      return self::log(logLevel::strWarn, $message);
+    }
+  }
+
+  public static function error($message)
+  {
+    if (self::getLogLevel() >= logLevel::ERROR) {
+      return self::log(logLevel::strError, $message);
+    }
+  }
+
+  public static function debug($message)
+  {
+    if (self::getLogLevel() >= logLevel::DEBUG) {
+      return self::log(logLevel::strDebug, $message);
+    }
+  }
+
+  private static function log($level, $message)
   {
   	$logger = fopen(self::getLogFile(), 'a');
-  	$result = (bool)fwrite($logger, self::buildLogMessage($message) . PHP_EOL);
+  	$result = (bool)fwrite($logger, self::buildLogMessage($level, $message) . PHP_EOL);
   	fclose($logger);
   	return $result;
   }
